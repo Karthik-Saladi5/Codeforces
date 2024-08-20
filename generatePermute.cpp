4bf1f84170030fa56121b4566d8d875ffd884af5
SK's Laptop <karthiksaladi6@gmail.com>
2024-08-21 00:17:40 +0530

    20-08-2024 contest

---
+++ generatePermute.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+#define pb push_back
+#define F first
+#define S second
+#define SQ(a) (a)*(a)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define ALL(a) (a).begin(), (a).end()
+const int mod = 1000000007;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define tc\
+    int t;\
+    cin>>t;\
+    while(t--)
+void takeinput(){
+    #ifndef ONLINE_JUDGE
+    freopen("D:\\coding\\input.txt","r",stdin);
+    freopen("D:\\coding\\output.txt","w",stdout);
+    #endif
+}
+int root(char key) {
+  if (tree[0] != '\0')
+    cout << "Tree already had root";
+  else
+    tree[0] = key;
+  return 0;
+}
+ 
+int set_left(char key, int parent) {
+  if (tree[parent] == '\0')
+    cout << "\nCan't set child at "
+    << (parent * 2) + 1
+    << " , no parent found";
+  else
+    tree[(parent * 2) + 1] = key;
+  return 0;
+}
+ 
+int set_right(char key, int parent) {
+  if (tree[parent] == '\0')
+    cout << "\nCan't set child at "
+    << (parent * 2) + 2
+    << " , no parent found";
+  else
+    tree[(parent * 2) + 2] = key;
+  return 0;
+}
+void sk() {
+    // Your code goes here
+    int n;
+    cin >> n;
+    if (n & 1) {
+        for (int i = 1;i <= n / 2;i++) cout << i << ' ';
+        for (int i = n;i > n / 2;i--) cout << i << ' ';
+        cout << endl;
+    }
+    else cout << "-1" << endl;
+}
+
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   takeinput();
+    tc sk();
+    return 0;
+}
