657dee56e88d4f4c3313d4b3985889aa6e952a33
SK's Laptop <karthiksaladi6@gmail.com>
2024-10-20 14:35:57 +0530

    19-10-2024 (div-2)

---
+++ aTrueBattle.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+#define pb push_back
+#define F first
+#define S second
+#define SQ(a) (a)*(a)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define ALL(a) (a).begin(), (a).end()
+const int mod = 1000000007;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define tc\
+    int t;\
+    cin>>t;\
+    while(t--)
+void takeinput(){
+    #ifndef ONLINE_JUDGE
+    freopen("D:\\coding\\input.txt","r",stdin);
+    freopen("D:\\coding\\output.txt","w",stdout);
+    freopen("D:\\coding\\error.txt","w",stderr);
+    #endif
+}
+void sk(){
+    // Your code goes here
+    ll n;cin >> n;
+    string s; cin >> s;
+    // int a = s[n - 2] - '0', b = s[n - 1] - '0';
+    bool flag = 0;
+    
+    no;
+    // if (n & 1) {
+    //     if (a & b or a | b) yes;
+    //     else no;
+    // }
+    // else {
+    //     if (a & b or a | b) yes;
+    //     else no;   
+    // }
+}
+
+int main()
+{
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   takeinput();
+   tc sk();
+    return 0;
+}
