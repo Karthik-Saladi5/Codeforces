1769d1a70713b17027da6db220d9614e5be8e76d
SK's Laptop <karthiksaladi6@gmail.com>
2024-12-23 13:59:40 +0530

    inputs and outputs added to template

--- cpptemplate.cpp
+++ cpptemplate.cpp
@@ -1,22 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+/*<-------alternates---------->*/
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vi;
+typedef vector<ld> vd;
+typedef pair<ll,ll> pi;
 #define pb push_back
+#define F first
+#define S second
+#define int ll
+/*<--------------------------->*/
+/*<----method shortcuts------->*/
+#define SQ(a) (a)*(a)
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+#define ALL(a) (a).begin(), (a).end()
+#define fixed(n) fixed<<setprecision(n)
+#define middle low+(high-low)/2
+/*<---------------------------------->*/
+/*<------------input----------------->*/
+template <class ...As> auto& read(As &...as) { return (cin >> ... >> as); }
+template <class T> auto &operator>>(istream &is, vector<T> &xs) {
+    for (auto &x : xs) is >> x;
+    return is;
+}
+#define In(...)             __VA_ARGS__; read(__VA_ARGS__)
+#define ArIn(name, ...)  name(__VA_ARGS__); cin >> name
+/*<----------------------------------->*/
+/*<------------output----------------->*/
+template <class ...As> auto& write(As ...as) {return (cout << ... << as);}
+template <class T> auto& operator<<(ostream& os, const vector<T>& xs) { for (const auto& x : xs) os << x << ' '; return os;}
+#define Out(...)              write(__VA_ARGS__)
+/*<------------debug----------------->*/
+
+/*<----------------------------------->*/
+/*<----user defined defaults---------->*/
 const int mod = 1000000007;
 #define yes cout<<"YES"<<endl
 #define no cout<<"NO"<<endl
 #define tc\
-        int t;\
-        cin>>t;\
-        while(t--)
+    int t;\
+    cin>>t;\
+    while(t--)
 void takeinput(){
-   #ifndef ONLINE_JUDGE
-    freopen("D:\\coding\\input.txt","r",stdin);
-    freopen("D:\\coding\\output.txt","w",stdout);
+    #ifndef ONLINE_JUDGE
+    freopen("D:/coding/input.txt","r",stdin);
+    freopen("D:/coding/output.txt","w",stdout);
+    freopen("D:/coding/error.txt","w",stderr);
     #endif
 }
+///// be an EGOIST/////////////////////////////
+void sk() {
+    // Your code goes here
+    
+}
+///// be an EGOIST/////////////////////////////
+#undef int
 int main()
 {
-    takeinput();
-    
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   takeinput();
+    sk();
+    return 0;
 }
